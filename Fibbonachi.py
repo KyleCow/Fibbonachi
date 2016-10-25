912436239411294dd7c3e97d30be1c70b227579e
KyleCow <9131@kingdown.wilts.sch.uk>
2016-10-25 18:31:38 +0100

    Create Fibbonachi.py

---
+++ Fibbonachi.py
@@ -0,0 +1,10 @@
+number=1
+number1=1
+while number > 0:
+    print(number)
+    print(number+number1)
+    number3=number+number1
+    number1=number
+    print(number3+number1)
+    number=number3+number1
+    number1=number3
