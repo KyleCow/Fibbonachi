cc992d047c47c087e65d765039e22210e47c73c9
KyleCow <9131@kingdown.wilts.sch.uk>
2017-01-01 18:10:37 +0000

    Update Fibbonachi.py

--- Fibbonachi.py
+++ Fibbonachi.py
@@ -1,10 +1,9 @@
-number=1
-number1=1
-while number > 0:
+num = 0
+number = 1
+
+while 1 == 1:
+    print(num)
+    number = number + num
     print(number)
-    print(number+number1)
-    number3=number+number1
-    number1=number
-    print(number3+number1)
-    number=number3+number1
-    number1=number3
+    num = num + number
+    
