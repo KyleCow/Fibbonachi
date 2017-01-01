04351a7f81ba6b5628dd39e21f9b4027bed84f6e
KyleCow <9131@kingdown.wilts.sch.uk>
2017-01-01 18:15:34 +0000

    Update Fibbonachi.py

--- Fibbonachi.py
+++ Fibbonachi.py
@@ -1,8 +1,12 @@
-num = input("number 1  ")
-number = input("number 2  ")
+num = 1
+number = 2
 
-while 1 == 1:
-    print(num)
-    number = number + num
-    print(number)
-    num = num + number
+while num > number:
+    num = input("number 1  ")
+    number = input("number 2  ")
+
+    while 1 == 1:
+        print(num)
+        number = number + num
+        print(number)
+        num = num + number
